36d24b0c6369c5708a8ec418231916266a0902cd
Ernesto Camacho <ernesto.camachoa9805gmail.com>
2021-09-23 10:02:37 -0500

    start of mongo operations

--- src/main/java/edu/escuelaing/tallerdocker/SparkWebServer.java
+++ src/main/java/edu/escuelaing/tallerdocker/SparkWebServer.java
@@ -3,11 +3,53 @@ package edu.escuelaing.tallerdocker;
 import static spark.Spark.get;
 import static spark.Spark.port;
 
+import java.net.UnknownHostException;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+import spark.Request;
+import spark.Response;
+
+
+import org.json.JSONObject;
+
+
 public class SparkWebServer {
-    
-    public static void main(String... args){
-          port(getPort());
-          get("hello", (req,res) -> "Hello Docker!");
+
+    private static MongoClient mongoClient;
+    public static void main(String... args) {
+        port(getPort());
+
+        mongoClient();
+
+        get("hello", (req,res) -> "Hello Docker!");
+        get("/logservice", (req,res) -> logServiceResponse(req,res));
+    }
+
+    private static void mongoClient(){
+        try {
+            mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean dataBaseSave(String cadena){
+        DB database= mongoClient.getDB("cadenas");
+        DBCollection collection= database.getCollection("cadenas");
+        DBObject cadenaAGuardar= new BasicDBObject("cadena",cadena);
+        collection.insert(cadenaAGuardar);
+        return false;
+    }
+    private static JSONObject logServiceResponse(Request req, Response res){
+        res.type("application/json");
+        String cadena= req.queryParams("cadena");
+        return null;
     }
 
     private static int getPort() {
